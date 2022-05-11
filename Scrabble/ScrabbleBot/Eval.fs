b29cf4ee297cc2e49947a3a9d7a6f6f7e91d5bdd
ivtarasyuk <ivtarasyuk@ivtarasyuks-MacBook-Air.local>
2022-05-11 15:58:59 +0200

    Eval

--- Scrabble/ScrabbleBot/Eval.fs
+++ Scrabble/ScrabbleBot/Eval.fs
@@ -1,16 +1,35 @@
-﻿// Insert your updated Eval.fs file here from Assignment 7. All modules must be internal.
-
-module internal Eval
+﻿module internal Eval
 
     open StateMonad
 
-    let add a b = a >>= (fun v1 -> b >>= (fun v2 -> ret (v1 + v2)))    
+    (* Code for testing *)
+
+    let hello = [('h', 4); ('e', 1); ('l', 1); ('l', 1); ('O', 1)]
+    let state = mkState [("x", 5); ("y", 42)] hello ["_pos_"; "_result_"]
+    let emptyState = mkState [] [] []
+    
+    let add (a: SM<int>) (b: SM<int>) =  a >>= (fun v1 -> b >>= (fun v2 -> ret (v1 + v2))) 
     let div a b = a >>= (fun v1 -> b
                                    >>= (fun v2 ->
                                       if (v2 <> 0) then
                                           ret (v1 / v2)
                                       else
-                                          fail DivisionByZero))     
+                                          fail DivisionByZero))
+    let sub (a: SM<int>) (b: SM<int>) : SM<int> = 
+        a >>= (fun v1 -> b >>= (fun v2 -> ret (v1-v2)))
+
+    let mul (a: SM<int>) (b: SM<int>) : SM<int> = 
+        a >>= (fun v1 -> b >>= (fun v2 -> ret (v1*v2)))
+    let modulo a b = 
+        a >>= (fun v1 -> b >>= (fun v2 ->
+                                          if (v2 <> 0) then
+                                            ret (v1 % v2)
+                                          else
+                                            fail DivisionByZero))
+     
+    let eql a b = a >>= (fun v1 -> b >>= (fun v2 -> ret (v1 = v2)))
+    let lt a b = a >>= (fun v1 -> b >>= (fun v2 -> ret (v1 < v2)))
+    let conj a b = a >>= (fun v1 -> b >>= (fun v2 -> ret (v1 && v2)))
 
     type aExp =
         | N of int
@@ -63,11 +82,37 @@ module internal Eval
     let (.>=.) a b = ~~(a .<. b)                (* numeric greater than or equal to *)
     let (.>.) a b = ~~(a .=. b) .&&. (a .>=. b) (* numeric greater than *)    
 
-    let arithEval a : SM<int> = failwith "Not implemented"      
+    let rec arithEval a : SM<int> =
+        match a with
+        | N n -> ret n
+        | V v -> lookup v
+        | WL -> wordLength
+        | PV pv -> arithEval pv >>= pointValue
+        | Add (a,b) -> add (arithEval a) (arithEval b)
+        | Sub (a,b) -> sub (arithEval a) (arithEval b)
+        | Mul (a,b) -> mul (arithEval a) (arithEval b)
+        | Div (a,b) -> div (arithEval a) (arithEval b)
+        | Mod (a,b) -> modulo (arithEval a) (arithEval b)
+        | CharToInt c -> charEval c >>= (fun x -> ret (int(x)))     
 
-    let charEval c : SM<char> = failwith "Not implemented"      
-
-    let boolEval b : SM<bool> = failwith "Not implemented"
+    and charEval c : SM<char> =
+        match c with
+        | C c -> ret c
+        | CV cv -> arithEval cv >>= characterValue
+        | ToUpper c -> charEval c >>= (fun x -> ret (System.Char.ToUpper x))
+        | ToLower c -> charEval c >>= (fun x -> ret (System.Char.ToLower x))
+        | IntToChar a -> arithEval a >>= (fun x -> ret (char(x)))
+                                          
+    and boolEval b : SM<bool> =
+        match b with
+        | TT -> ret true
+        | FF -> ret false
+        | AEq (a, b) -> eql (arithEval a) (arithEval b)
+        | ALt (a, b) -> lt (arithEval a) (arithEval b)
+        | Not a -> (boolEval a) >>= (fun x -> ret (not x))
+        | Conj (a, b) -> conj (boolEval a) (boolEval b)
+        | IsLetter a -> (charEval a) >>= (fun x -> ret (System.Char.IsLetter x))
+        | IsDigit a -> (charEval a) >>= (fun x -> ret (System.Char.IsDigit x))
 
 
     type stm =                (* statements *)
@@ -78,44 +123,75 @@ module internal Eval
     | ITE of bExp * stm * stm (* if-then-else statement *)
     | While of bExp * stm     (* while statement *)
 
-    let rec stmntEval stmnt : SM<unit> = failwith "Not implemented"
+    let rec stmntEval stmnt : SM<unit> =
+        match stmnt with
+        | Declare var -> declare var
+        | Ass (var, value) -> (arithEval value) >>= update var
+        | Skip -> ret()
+        | Seq (s1, s2) -> stmntEval s1 >>>= stmntEval s2
+        | ITE (b, s1, s2) -> boolEval b >>= (fun x -> if x
+                                                        then stmntEval s1
+                                                        else stmntEval s2)
+        | While (b, s) -> boolEval b >>= (fun x -> if x
+                                                    then stmntEval s
+                                                    >>>= stmntEval(While(b, s))
+                                                    else stmntEval(Skip))
+       
 
 (* Part 3 (Optional) *)
 
-    type StateBuilder() =
+//    type StateBuilder() =
+//
+//        member this.Bind(f, x)    = f >>= x
+//        member this.Return(x)     = ret x
+//        member this.ReturnFrom(x) = x
+//        member this.Delay(f)      = f ()
+//        member this.Combine(a, b) = a >>= (fun _ -> b)
+//        
+//    let prog = new StateBuilder()
+//
+//    let arithEval2 a = failwith "Not implemented"
+//    let charEval2 c = failwith "Not implemented"
+//    let rec boolEval2 b = failwith "Not implemented"
+//
+//    let stmntEval2 stm = failwith "Not implemented"
 
-        member this.Bind(f, x)    = f >>= x
-        member this.Return(x)     = ret x
-        member this.ReturnFrom(x) = x
-        member this.Delay(f)      = f ()
-        member this.Combine(a, b) = a >>= (fun _ -> b)
-        
-    let prog = new StateBuilder()
-
-    let arithEval2 a = failwith "Not implemented"
-    let charEval2 c = failwith "Not implemented"
-    let rec boolEval2 b = failwith "Not implemented"
-
-    let stmntEval2 stm = failwith "Not implemented"
-
-(* Part 4 *) 
+(* Part 4 (Optional) *) 
 
     type word = (char * int) list
     type squareFun = word -> int -> int -> Result<int, Error>
-
-    let stmntToSquareFun stm = failwith "Not implemented"
-
+    //type squareFun = word -> int -> int -> int
 
     type coord = int * int
 
-    type boardFun = coord -> Result<squareFun option, Error> 
-
-    let stmntToBoardFun stm m = failwith "Not implemented"
+    //type boardFun = coord -> Result<squareFun option, Error>
+    type boardFun = coord -> squareFun option
 
     type board = {
         center        : coord
         defaultSquare : squareFun
         squares       : boardFun
     }
+//    let mkBoard c defaultSq boardStmnt ids = {
+//        center = c; 
+//        defaultSquare = stmntToSquareFun defaultSq; 
+//        squares = List.map (fun (x, y) -> stmntToSquareFun y) ids |> Map.ofList  |> stmntToBoardFun boardStmnt
+//        }
+    let stmntToSquareFun =
+        fun stm w pos acc ->
+            mkState [("_pos_", pos); ("_acc_", acc); ("_result_", 0)] w ["_pos_"; "_acc_"; "_result_"]
+            |> fun s -> evalSM s (stmntEval stm >>>= lookup "_result_")
 
-    let mkBoard c defaultSq boardStmnt ids = failwith "Not implemented"
+    let boardState x y =
+        mkState [("_x_", x); ("_y_", y); ("_result_", 0)] [] [ "_x_"; "_y_"; "_result_" ]
+       
+    let stmntToBoardFun stm squares =
+        let boardfun (x, y) =
+            let sm = stmntEval stm >>>= lookup "_result_" >>= fun id ->
+                match Map.tryFind id squares with
+                    | Some i -> ret (Some i)
+                    | _ -> ret None
+            match (evalSM (boardState x y) sm) with
+                | Success(x) -> x
+                | Failure(_) -> None
+        boardfun
--- Scrabble/ScrabbleBot/Parser.fs
+++ Scrabble/ScrabbleBot/Parser.fs
@@ -31,11 +31,11 @@ module internal Parser
     let pdo       = pstring "do"
     let pdeclare  = pstring "declare"
 
-    let whitespaceChar = satisfy System.Char.IsWhiteSpace <?> "whitespace"
-    let pletter        = satisfy System.Char.IsLetter <?> "letter"
+    let whitespaceChar = satisfy Char.IsWhiteSpace <?> "whitespace"
+    let pletter        = satisfy Char.IsLetter <?> "letter"
     
-    let palphanumeric  = satisfy System.Char.IsLetterOrDigit <?> "alphanumeric"
-    let pletters = many palphanumeric |>> fun (charList) -> System.String.Concat(Array.ofList(charList))
+    let palphanumeric  = satisfy Char.IsLetterOrDigit <?> "alphanumeric"
+    let pletters = many palphanumeric |>> fun (charList) -> String.Concat(Array.ofList(charList))
     
 
     let anyOf ss =
@@ -52,9 +52,10 @@ module internal Parser
     let (.>*>.) p1 p2 = (p1 .>> spaces) .>>. p2 
     let (.>*>) p1 p2  = (p1 .>> spaces) .>> p2
     let (>*>.) p1 p2  = (p1 .>> spaces) >>. p2
-    let parenthesise p = pchar '(' >*>. p .>*> pchar ')' 
-    let second_part = many (palphanumeric <|> underscore)  |>> fun (charList) -> System.String.Concat(Array.ofList(charList))
-    let pid = pletter <|> underscore .>>. second_part |>> fun (c1, s2) -> System.String.Concat(c1, s2)
+    let parenthesise p = pchar '(' >*>. p .>*> pchar ')'
+    let braces p = pchar '{' >*>. p .>*> pchar '}' 
+    let second_part = many (palphanumeric <|> underscore)  |>> fun (charList) -> String.Concat(Array.ofList(charList))
+    let pid = pletter <|> underscore .>>. second_part |>> fun (c1, s2) -> String.Concat(c1, s2)
 
     
     let unop parser1 parser2 =
@@ -64,9 +65,10 @@ module internal Parser
     let TermParse, tref = createParserForwardedToRef<aExp>()
     let ProdParse, pref = createParserForwardedToRef<aExp>()
     let AtomParse, aref = createParserForwardedToRef<aExp>()
-    let AexpParse = TermParse
-    
     let CharParse, charref = createParserForwardedToRef<cExp>()
+    
+    let AexpParse = TermParse
+    let CexpParse = CharParse
 
     let AddParse = binop (pchar '+') ProdParse TermParse |>> Add <?> "Add"
     let SubParse = binop (pchar '-') ProdParse TermParse |>> Sub <?> "Sub"
@@ -76,12 +78,10 @@ module internal Parser
     let DivParse = binop (pchar '/') AtomParse ProdParse |>> Div <?> "Div"
     let ModParse = binop (pchar '%') AtomParse ProdParse |>> Mod <?> "Mod"
     do pref := choice [MulParse; DivParse; ModParse; AtomParse]
-    
-    
-    // <?> in case of a failure write to the console a message "Int"
+     
+    // <?> in case of failure write to the console a message "Int"
     let NParse   = pint32 |>> N <?> "Int"
     let VParse =  pid |>> V <?> "Var"
-  
     let NegParse = pchar '-' >>. AexpParse |>> (fun x -> Mul (N -1, x)) <?>  "Negation"
     //let NegParse = pchar '-' >>. pint32 |>> (fun x -> Mul (N -1, N x)) <?>  "Negation"
     let PVParse = pstring "pointValue"  >*>. parenthesise AexpParse |>> PV <?> "PV"
@@ -89,19 +89,67 @@ module internal Parser
     let ParParse = parenthesise TermParse
     do aref := choice [CharToInt; NegParse; PVParse; NParse; VParse; ParParse]
     
-    let CexpParse = CharParse
     let CParse = pchar ''' >>. anyChar .>> pchar ''' |>> C <?> "C"
     let LowerParse = pstring "toLower" >*>. parenthesise CexpParse |>> ToLower <?> "toLower"
     let UpperParse = pstring "toUpper" >*>. parenthesise CexpParse |>> ToUpper <?> "toUpper"
     let CharValue = pstring "charValue" >*>. parenthesise AexpParse |>> CV <?> "CV"
     let IntToChar = pstring "intToChar" >*>. parenthesise AexpParse |>> IntToChar <?> "IntToChar"
-     
     do charref := choice [CParse; LowerParse; UpperParse; CharValue; IntToChar]
 
-    let BexpParse = pstring "not implemented"
+    let BTermParse, btref = createParserForwardedToRef<bExp>()
+    let BProdParse, bpref = createParserForwardedToRef<bExp>()
+    // do I need BAtomParse?
+    let BAtomParse, baref = createParserForwardedToRef<bExp>()
+    
+    let BexpParse = BTermParse
+    
+    // or BAtom, BProd
+    let ConjParse = binop (pstring @"/\") BProdParse BTermParse |>> Conj <?> "Conjunction"
+    let DisjParse = binop (pstring @"\/") BProdParse BTermParse |>> (fun (x, y) -> Not(Conj(Not x, Not y))) <?> "Disjunction"
+    do btref := choice [ConjParse; DisjParse; BProdParse]
+    
+    
+    let EqParse = binop (pchar '=') AtomParse ProdParse |>> AEq <?> "Equal"
+    let NotEqParse = binop (pstring @"<>") AtomParse ProdParse |>> (fun (x, y) -> Not(AEq(x, y))) <?> "Not equal"
+    let LessParse = binop (pchar '<') AtomParse ProdParse |>> ALt <?> "Less than"
+    // Conj(Not(ALt (x, y)), Not(AEq (x,y))) -> not "<" and not "="
+    // Not(Conj(Not(ALt (x, y)), Not(AEq (x,y)))))  -> not (not "<" and not "=") == "<" or "="
+    // Not (Conj (Not (ALt (N 6,N 3)),Not (Not (Not (AEq (N 6,N 3))))))
+    let LessOrEqParse = binop (pstring @"<=") AtomParse ProdParse |>> (fun (x, y) ->
+                                                                Not(Conj(Not(ALt (x, y)), Not(AEq (x,y))))) <?> "Less or Equal"
+    let MoreOrEqParse = binop (pstring @"=>") AtomParse ProdParse |>> (fun (x, y) -> Not(ALt (x, y))) <?> "More or equal"
+    // ">" == not "==" and not "<"
+    let MoreParse = binop (pchar '>') AtomParse ProdParse |>> fun (x,y) -> Conj (Not (AEq (x, y)), Not (ALt (x ,y)))
+    do bpref := choice [EqParse; NotEqParse; LessParse; LessOrEqParse; MoreOrEqParse; MoreParse; BAtomParse]
+    
+    let NotParse = unop (pchar '~') BexpParse |>> Not <?> "Not"
+    let TrueParse = pTrue |>> (fun _ -> TT) <?> "True"
+    let FalseParse = pFalse |>> (fun _ -> FF) <?> "False"
+    let BParParse = parenthesise BTermParse
+    do baref := choice [NotParse; TrueParse; FalseParse; BParParse]
+    
+    
+    /// let stmTermParse, stmtref = createParserForwardedToRef<stm>()
+   // let stmProdParse, stmpref = createParserForwardedToRef<stm>()
 
-    let stmParse = pstring "not implemented"
-
+    //let stmParse = stmTermParse
+    let stmParse, stmref = createParserForwardedToRef<stm>()
+    
+    let SeqParse = stmParse .>*> pchar ';' .>*>. stmParse |>> Seq <?> "Sequential"
+    // one space between the keyword and the identifier
+    let DeclareParse = pdeclare .>>. whitespaceChar >*>. pid |>> Declare <?> "Declare"
+    let AssignParse =  binop (pstring ":=") pid AexpParse |>> Ass <?> "Ass"
+    let ITEParse = pif >*>. parenthesise BexpParse .>*> pthen
+                   .>*>. braces stmParse .>*> pelse .>*>. braces stmParse
+                   |>> (fun ((x, y), z) -> ITE(x, y, z)) <?> "If then else"
+    let ITParse = pif >*>. parenthesise BexpParse .>*> pthen
+                   .>*>. braces stmParse
+                   |>> (fun (x, y) -> ITE(x, y, Skip)) <?> "If then"
+                   
+    let WhileParse = pwhile >*>. parenthesise BexpParse .>*> pdo
+                     .>*>. braces stmParse
+                     |>> While <?> "While"
+    do stmref := choice [SeqParse; DeclareParse; AssignParse; ITEParse; ITParse; WhileParse]
     (* The rest of your parser goes here *)
 
     type word   = (char * int) list
